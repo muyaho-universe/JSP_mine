55e5bc459954e68ee2c37071e6276a13f39aab3a
DaleKim <97732494+muyaho-universe@users.noreply.github.com>
2022-11-17 10:38:36 +0900

    feat: BoardDAO.java new  method

--- src/main/java/com/example/bean/BoardVO.java
+++ src/main/java/com/example/bean/BoardVO.java
@@ -76,4 +76,12 @@ public class BoardVO {
     public void setFileName(String fileName) {
         this.fileName = fileName;
     }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
--- src/main/java/com/example/dao/BoardDAO.java
+++ src/main/java/com/example/dao/BoardDAO.java
@@ -15,8 +15,8 @@ public class BoardDAO {
 	PreparedStatement stmt = null;
 	ResultSet rs = null;
 
-	private final String BOARD_INSERT = "insert into BOARD (title, writer, content) values (?,?,?)";
-	private final String BOARD_UPDATE = "update BOARD set title=?, writer=?, content=? where seq=?";
+	private final String BOARD_INSERT = "insert into BOARD (category, title, writer, content, fileName) values (?, ?, ?, ?, ?)";
+	private final String BOARD_UPDATE = "update BOARD set category=?, title=?, writer=?, content=?, fileName=? where seq=?";
 	private final String BOARD_DELETE = "delete from BOARD  where seq=?";
 	private final String BOARD_GET = "select * from BOARD  where seq=?";
 	private final String BOARD_LIST = "select * from BOARD order by seq desc";
@@ -30,16 +30,12 @@ public class BoardDAO {
 		try {
 			conn = JDBCUtil2.getConnection();
 			stmt = conn.prepareStatement(BOARD_INSERT);
-			stmt.setString(1, vo.getTitle());
-			stmt.setString(2, vo.getWriter());
-			stmt.setString(3, vo.getContent());
-			stmt.executeUpdate();
-			stmt = conn.prepareStatement(BOARD_INSERT);
 			stmt.setString(1, vo.getCategory());
 			stmt.setString(2, vo.getTitle());
 			stmt.setString(3, vo.getWriter());
 			stmt.setString(4, vo.getContent());
 			stmt.setString(5, vo.getFileName());
+			stmt.executeUpdate();
 			return 1;
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -64,10 +60,12 @@ public class BoardDAO {
 		try {
 			conn = JDBCUtil2.getConnection();
 			stmt = conn.prepareStatement(BOARD_UPDATE);
-			stmt.setString(1, vo.getTitle());
-			stmt.setString(2, vo.getWriter());
-			stmt.setString(3, vo.getContent());
-			stmt.setInt(4, vo.getSeq());
+			stmt.setString(1, vo.getCategory());
+			stmt.setString(2, vo.getTitle());
+			stmt.setString(3, vo.getWriter());
+			stmt.setString(4, vo.getContent());
+			stmt.setString(5, vo.getFileName());
+			stmt.setInt(6, vo.getSeq());
 			
 			
 			System.out.println(vo.getTitle() + "-" + vo.getWriter() + "-" + vo.getContent() + "-" + vo.getSeq());
@@ -124,5 +122,22 @@ public class BoardDAO {
 			e.printStackTrace();
 		} 
 		return list;
+	}
+	public String getPhotoFilename(int sid) {
+		String filename = null;
+		try {
+			conn = JDBCUtil2.getConnection();
+			stmt = conn.prepareStatement(BOARD_GET);
+			stmt.setInt(1, sid);
+			rs = stmt.executeQuery();
+			if(rs.next()) {
+				filename = rs.getString("photo");
+			}
+			rs.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("==> JDBC로 getPhotoFilename() 기능 처리");
+		return filename;
 	}
 }
