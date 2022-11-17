e6bb8c3f7d761c6ed80d3701006b03f4ffa742f3
DaleKim <97732494+muyaho-universe@users.noreply.github.com>
2022-11-17 21:17:32 +0900

    working on edit

--- src/main/java/com/example/dao/BoardDAO.java
+++ src/main/java/com/example/dao/BoardDAO.java
@@ -6,142 +6,147 @@ import com.example.util.JDBCUtil2;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BoardDAO {
-	
-	Connection conn = null;
-	PreparedStatement stmt = null;
-	ResultSet rs = null;
 
-	private final String BOARD_INSERT = "insert into BOARD (category, title, writer, content, fileName) values (?, ?, ?, ?, ?)";
-	private final String BOARD_UPDATE = "update BOARD set category=?, title=?, writer=?, content=?, fileName=? where seq=?";
-	private final String BOARD_DELETE = "delete from BOARD  where seq=?";
-	private final String BOARD_GET = "select * from BOARD  where seq=?";
-	private final String BOARD_LIST = "select * from BOARD order by seq desc";
+    Connection conn = null;
+    PreparedStatement stmt = null;
+    ResultSet rs = null;
 
-	Connection connection = null;
-	PreparedStatement preparedStatement = null;
-	ResultSet resultSet = null;
+    private final String BOARD_INSERT = "insert into BOARD (category, title, writer, content, fileName) values (?, ?, ?, ?, ?)";
+    private final String BOARD_UPDATE = "update BOARD set category=?, title=?, writer=?, content=?, fileName=?, editdate=? where seq=?";
+    private final String BOARD_DELETE = "delete from BOARD  where seq=?";
+    private final String BOARD_GET = "select * from BOARD  where seq=?";
+    private final String BOARD_LIST = "select * from BOARD order by seq desc";
+
+    Connection connection = null;
+    PreparedStatement preparedStatement = null;
+    ResultSet resultSet = null;
 
 
-	public int insertBoard(BoardVO vo) {
-		System.out.println("===> JDBC로 insertBoard() 기능 처리");
-		try {
-			conn = JDBCUtil2.getConnection();
-			stmt = conn.prepareStatement(BOARD_INSERT);
-			stmt.setString(1, vo.getCategory());
-			stmt.setString(2, vo.getTitle());
-			stmt.setString(3, vo.getWriter());
-			stmt.setString(4, vo.getContent());
-			stmt.setString(5, vo.getFileName());
-			stmt.executeUpdate();
-			return 1;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return 0;
-	}
+    public int insertBoard(BoardVO vo) {
+        System.out.println("===> JDBC로 insertBoard() 기능 처리");
+        try {
+            conn = JDBCUtil2.getConnection();
+            stmt = conn.prepareStatement(BOARD_INSERT);
+            stmt.setString(1, vo.getCategory());
+            stmt.setString(2, vo.getTitle());
+            stmt.setString(3, vo.getWriter());
+            stmt.setString(4, vo.getContent());
+            stmt.setString(5, vo.getFileName());
+            stmt.executeUpdate();
+            return 1;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
 
-	// 글 삭제
-	public void deleteBoard(BoardVO vo) {
-		System.out.println("===> JDBC로 deleteBoard() 기능 처리");
-		try {
-			conn = JDBCUtil2.getConnection();
-			stmt = conn.prepareStatement(BOARD_DELETE);
-			stmt.setInt(1, vo.getSeq());
-			stmt.executeUpdate();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	public int updateBoard(BoardVO vo) {
-		System.out.println("===> JDBC로 updateBoard() 기능 처리");
-		try {
-			conn = JDBCUtil2.getConnection();
-			stmt = conn.prepareStatement(BOARD_UPDATE);
-			stmt.setString(1, vo.getCategory());
-			stmt.setString(2, vo.getTitle());
-			stmt.setString(3, vo.getWriter());
-			stmt.setString(4, vo.getContent());
-			stmt.setString(5, vo.getFileName());
-			stmt.setInt(6, vo.getSeq());
-			
-			
-			System.out.println(vo.getTitle() + "-" + vo.getWriter() + "-" + vo.getContent() + "-" + vo.getSeq());
-			stmt.executeUpdate();
-			return 1;
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return 0;
-	}	
-	
-	public BoardVO getBoard(int seq) {
-		BoardVO one = new BoardVO();
-		System.out.println("===> JDBC로 getBoard() 기능 처리");
-		try {
-			conn = JDBCUtil2.getConnection();
-			stmt = conn.prepareStatement(BOARD_GET);
-			stmt.setInt(1, seq);
-			rs = stmt.executeQuery();
-			if(rs.next()) {
-				one.setSeq(rs.getInt("seq"));
-				one.setTitle(rs.getString("title"));
-				one.setWriter(rs.getString("writer"));
-				one.setContent(rs.getString("content"));
-				one.setCnt(rs.getInt("cnt"));
-			}
-			rs.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return one;
-	}
-	
-	public List<BoardVO> getBoardList(){
-		List<BoardVO> list = new ArrayList<BoardVO>();
-		System.out.println("===> JDBC로 getBoardList() 기능 처리");
-		try {
-			conn = JDBCUtil2.getConnection();
-			stmt = conn.prepareStatement(BOARD_LIST);
-			rs = stmt.executeQuery();
-			while(rs.next()) {
-				BoardVO one = new BoardVO();
-				one.setSeq(rs.getInt("seq"));
-				one.setCategory(rs.getString("category"));
-				one.setTitle(rs.getString("title"));
-				one.setWriter(rs.getString("writer"));
-				one.setContent(rs.getString("content"));
-				one.setFileName(rs.getString("fileName"));
-				one.setRegdate(rs.getDate("regdate"));
-				one.setEditdate(rs.getDate("editdate"));
-				one.setCnt(rs.getInt("cnt"));
-				list.add(one);
-			}
-			rs.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} 
-		return list;
-	}
-	public String getPhotoFilename(int sid) {
-		String filename = null;
-		try {
-			conn = JDBCUtil2.getConnection();
-			stmt = conn.prepareStatement(BOARD_GET);
-			stmt.setInt(1, sid);
-			rs = stmt.executeQuery();
-			if(rs.next()) {
-				filename = rs.getString("photo");
-			}
-			rs.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		System.out.println("==> JDBC로 getPhotoFilename() 기능 처리");
-		return filename;
-	}
+    // 글 삭제
+    public void deleteBoard(BoardVO vo) {
+        System.out.println("===> JDBC로 deleteBoard() 기능 처리");
+        try {
+            conn = JDBCUtil2.getConnection();
+            stmt = conn.prepareStatement(BOARD_DELETE);
+            stmt.setInt(1, vo.getSeq());
+            stmt.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int updateBoard(BoardVO vo) {
+        System.out.println("===> JDBC로 updateBoard() 기능 처리");
+        try {
+            conn = JDBCUtil2.getConnection();
+            stmt = conn.prepareStatement(BOARD_UPDATE);
+            stmt.setString(1, vo.getCategory());
+            stmt.setString(2, vo.getTitle());
+            stmt.setString(3, vo.getWriter());
+            stmt.setString(4, vo.getContent());
+            stmt.setString(5, vo.getFileName());
+            stmt.setTimestamp(6, new Timestamp(System.currentTimeMillis()));
+
+            stmt.setInt(7, vo.getSeq());
+
+
+            System.out.println(vo.getTitle() + "-" + vo.getWriter() + "-" + vo.getContent() + "-" + vo.getFileName() + "-" + vo.getSeq());
+            stmt.executeUpdate();
+            return 1;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public BoardVO getBoard(int seq) {
+        BoardVO one = new BoardVO();
+        System.out.println("===> JDBC로 getBoard() 기능 처리");
+        try {
+            conn = JDBCUtil2.getConnection();
+            stmt = conn.prepareStatement(BOARD_GET);
+            stmt.setInt(1, seq);
+            rs = stmt.executeQuery();
+            if (rs.next()) {
+                one.setSeq(rs.getInt("seq"));
+                one.setTitle(rs.getString("title"));
+                one.setWriter(rs.getString("writer"));
+                one.setContent(rs.getString("content"));
+                one.setCnt(rs.getInt("cnt"));
+            }
+            rs.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return one;
+    }
+
+    public List<BoardVO> getBoardList() {
+        List<BoardVO> list = new ArrayList<BoardVO>();
+        System.out.println("===> JDBC로 getBoardList() 기능 처리");
+        try {
+            conn = JDBCUtil2.getConnection();
+            stmt = conn.prepareStatement(BOARD_LIST);
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                BoardVO one = new BoardVO();
+                one.setSeq(rs.getInt("seq"));
+                one.setCategory(rs.getString("category"));
+                one.setTitle(rs.getString("title"));
+                one.setWriter(rs.getString("writer"));
+                one.setContent(rs.getString("content"));
+                one.setFileName(rs.getString("fileName"));
+                one.setRegdate(rs.getDate("regdate"));
+                one.setEditdate(rs.getDate("editdate"));
+                one.setCnt(rs.getInt("cnt"));
+                list.add(one);
+            }
+            rs.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public String getPhotoFilename(int sid) {
+        String filename = null;
+        try {
+            conn = JDBCUtil2.getConnection();
+            stmt = conn.prepareStatement(BOARD_GET);
+            stmt.setInt(1, sid);
+            rs = stmt.executeQuery();
+            if (rs.next()) {
+                filename = rs.getString("photo");
+            }
+            rs.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("==> JDBC로 getPhotoFilename() 기능 처리");
+        return filename;
+    }
 }
